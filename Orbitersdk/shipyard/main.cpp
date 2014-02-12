af118ad7d431241c080492c98093ce55bfda0989
Christopher Johnstone <cpj97@juno.com>
2014-02-11 21:05:26 -0500

    Added basic 3d-engine code

---
+++ Orbitersdk/shipyard/main.cpp
@@ -0,0 +1,30 @@
+#include <irrlicht.h>
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+using namespace irr;
+
+int main()
+{
+	// create a NULL device to detect screen resolution
+	IrrlichtDevice *nulldevice = createDevice(video::EDT_NULL);
+
+	core::dimension2d<u32> deskres = nulldevice->getVideoModeList()->getDesktopResolution();
+
+	nulldevice->drop();
+
+	IrrlichtDevice *device = createDevice(video::EDT_DIRECT3D9, deskres, 32, false, false, false, NULL);
+	if (!device)
+		return 1;
+
+	//set caption
+	device->setWindowCaption(L"Orbiter Shipyard");
+
+	while (device->run())
+	{
+
+	}
+	device->drop();
+} 
