36dc0227a5384d14fbbb7cbd5a4339d4e9ab1e67
Christopher Johnstone <cpj97@juno.com>
2014-02-16 16:19:47 -0500

    Added basic test code

--- Orbitersdk/shipyard/Shipyard.cpp
+++ Orbitersdk/shipyard/Shipyard.cpp
@@ -1,1 +1,25 @@
 #include "Shipyard.h"
+
+void Shipyard::loop()
+{
+	video::IVideoDriver* driver = device->getVideoDriver();
+	scene::ISceneManager* smgr = device->getSceneManager();
+	
+	//add the camera
+	smgr->addCameraSceneNodeMaya();
+
+	//register our VesselSceneNode - just staticly at the moment, but will do it later
+	VesselSceneNode* vesselNode = new VesselSceneNode(".\Config\Vessels\Deltaglider.cfg", 
+		smgr->getRootSceneNode(), smgr, 1);
+	vesselNode->drop();
+
+	//start the loop
+	while (device->run())
+	{
+		driver->beginScene(true, true, video::SColor(0, 100, 100, 100));
+
+		smgr->drawAll();
+
+		driver->endScene();
+	}
+}
