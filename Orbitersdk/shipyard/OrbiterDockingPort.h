a65d8a687e58638227381bcd117f4e9c13c4b4af
Christopher Johnstone <cpj97@juno.com>
2014-02-16 12:30:25 -0500

    Added constructor to struct OrbiterDockingPort

--- Orbitersdk/shipyard/OrbiterDockingPort.h
+++ Orbitersdk/shipyard/OrbiterDockingPort.h
@@ -4,6 +4,8 @@ using namespace irr;
 
 struct OrbiterDockingPort
 {
+	OrbiterDockingPort(core::vector3d<f32> pos, core::vector3d<f32> appDir, core::vector3d<f32> refDir)
+	: position(pos), approachDirection(appDir), referenceDirection(refDir) {}
 	core::vector3d<f32> position;
 	core::vector3d<f32> approachDirection;
 	core::vector3d<f32> referenceDirection;
