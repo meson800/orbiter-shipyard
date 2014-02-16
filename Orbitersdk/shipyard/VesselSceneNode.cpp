156b58799521f789c609d6e138a8ea72f611c846
Christopher Johnstone <cpj97@juno.com>
2014-02-16 12:30:42 -0500

    Added vessel loading code

--- Orbitersdk/shipyard/VesselSceneNode.cpp
+++ Orbitersdk/shipyard/VesselSceneNode.cpp
@@ -1,1 +1,44 @@
 #include "VesselSceneNode.h"
+
+VesselSceneNode::VesselSceneNode(string configFilename, scene::ISceneNode* parent, scene::ISceneManager* mgr, s32 id)
+: scene::ISceneNode(parent, mgr, id)
+{
+	vector<string> tokens;
+	ifstream configFile = ifstream(configFilename.c_str());
+
+	bool readingDockingPorts = false;
+
+	while (Helpers::readLine(configFile, tokens))
+	{
+		//if we are reading docking ports, create a new docking port!
+		if (readingDockingPorts)
+			dockingPorts.push_back(OrbiterDockingPort(
+			core::vector3d<f32>(Helpers::stringToDouble(tokens[1]),
+				Helpers::stringToDouble(tokens[2]), Helpers::stringToDouble(tokens[3])),
+			core::vector3d<f32>(Helpers::stringToDouble(tokens[4]),
+			Helpers::stringToDouble(tokens[5]), Helpers::stringToDouble(tokens[6])),
+				core::vector3d<f32>(Helpers::stringToDouble(tokens[7]),
+			Helpers::stringToDouble(tokens[8]), Helpers::stringToDouble(tokens[9]))));
+		//now see if this is the beginning of a docking port list
+		if (tokens[0].compare("BEGIN_DOCKLIST"))
+		{
+			readingDockingPorts = true;
+		}
+		//or if it is the end
+		if (tokens[0].compare("END_DOCKLIST"))
+			readingDockingPorts = false;
+
+		//now see if it is a MeshName
+		//put it in lowercase to start
+		transform(tokens[0].begin(), tokens[0].end(), tokens[0].begin(), ::tolower);
+		//see if it matches
+		if (tokens[0].compare("meshname"))
+			//load the mesh!
+			vesselMesh.setupMesh(string(".\\Meshes\\" + tokens[2]), mgr->getVideoDriver()); //tokens 2 because the format is
+		//MeshName = blahblah
+
+		//clear tokens
+		tokens.clear();
+
+	}
+}
--- Orbitersdk/shipyard/VesselSceneNode.h
+++ Orbitersdk/shipyard/VesselSceneNode.h
@@ -1,8 +1,22 @@
 #include <irrlicht.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+#include "OrbiterMesh.h"
+#include "Helpers.h"
+#include "OrbiterDockingPort.h"
 
 using namespace irr;
+using namespace std;
 
 class VesselSceneNode : public scene::ISceneNode
 {
+public:
+	VesselSceneNode(string configFilename, scene::ISceneNode* parent, scene::ISceneManager* mgr, s32 id);
 
+private:
+	vector<OrbiterDockingPort> dockingPorts;
+	OrbiterMesh vesselMesh;
 };
