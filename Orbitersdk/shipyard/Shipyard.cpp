1832391281134365c34a47a9b96746cbefaa8b14
Christopher Johnstone <cpj97@juno.com>
2014-02-17 16:19:51 -0500

    Changed test vessel to ProjectAlpha_ISS

--- Orbitersdk/shipyard/Shipyard.cpp
+++ Orbitersdk/shipyard/Shipyard.cpp
@@ -11,7 +11,7 @@ void Shipyard::loop()
 	smgr->setAmbientLight(SColor(150,150,150,150));
 
 	//register our VesselSceneNode - just staticly at the moment, but will do it later
-	VesselSceneNode* vesselNode = new VesselSceneNode("C:\\Other Stuff\\Orbiter\\shipyard\\Config\\Vessels\\ISS.cfg", 
+	VesselSceneNode* vesselNode = new VesselSceneNode("C:\\Other Stuff\\Orbiter\\shipyard\\Config\\Vessels\\ProjectAlpha_ISS.cfg", 
 		smgr->getRootSceneNode(), smgr, 1);
 	vesselNode->drop();
 
