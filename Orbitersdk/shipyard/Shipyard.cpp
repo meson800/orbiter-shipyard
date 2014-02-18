84ea6b93d185b78ce25544b11f2df5c799a8455a
Christopher Johnstone <cpj97@juno.com>
2014-02-18 11:52:08 -0500

    Added basic dragging ability to Shipyard

--- Orbitersdk/shipyard/Shipyard.cpp
+++ Orbitersdk/shipyard/Shipyard.cpp
@@ -1,9 +1,15 @@
 #include "Shipyard.h"
 
+void Shipyard::setupDevice(IrrlichtDevice * _device)
+{
+	device = _device;
+	smgr = device->getSceneManager();
+	collisionManager = smgr->getSceneCollisionManager();
+}
+
 void Shipyard::loop()
 {
 	video::IVideoDriver* driver = device->getVideoDriver();
-	scene::ISceneManager* smgr = device->getSceneManager();
 	
 	//add the camera
 	smgr->addCameraSceneNodeMaya();
@@ -12,7 +18,7 @@ void Shipyard::loop()
 
 	//register our VesselSceneNode - just staticly at the moment, but will do it later
 	VesselSceneNode* vesselNode = new VesselSceneNode("C:\\Other Stuff\\Orbiter\\shipyard\\Config\\Vessels\\ProjectAlpha_ISS.cfg", 
-		smgr->getRootSceneNode(), smgr, 1);
+		smgr->getRootSceneNode(), smgr, 72);
 	vesselNode->drop();
 
 	//start the loop
@@ -26,7 +32,65 @@ void Shipyard::loop()
 	}
 }
 
+core::vector3df Shipyard::returnMouseRelativePos()
+{
+	core::vector3df mouse3DPos;
+	if (selectedNode != 0)
+	{
+		//update the absolute position
+		selectedNode->updateAbsolutePosition();
+		//the plane point is from the center of the node, with the normal to the camera
+		core::plane3df plane = core::plane3df(selectedNode->getAbsolutePosition(),
+			collisionManager->getRayFromScreenCoordinates(collisionManager->getScreenCoordinatesFrom3DPosition(
+			selectedNode->getAbsolutePosition())).getVector());
+
+		//now do a ray from the current mouse position
+		core::line3df ray = collisionManager->getRayFromScreenCoordinates(device->getCursorControl()->getPosition());
+		//if they intersect, set it equal to mouse3DPos
+		plane.getIntersectionWithLine(ray.start, ray.getVector(), mouse3DPos);
+	}
+	return mouse3DPos;
+}
+
 bool Shipyard::OnEvent(const SEvent& event)
 {
+	switch (event.EventType)
+	{
+	case EET_MOUSE_INPUT_EVENT:
+		switch (event.MouseInput.Event)
+		{
+		case EMIE_LMOUSE_PRESSED_DOWN:
+			LMouseDown = true;
+			//try to select a node
+			selectedNode = collisionManager->getSceneNodeFromScreenCoordinatesBB(
+				device->getCursorControl()->getPosition(), 72, true);
+			//set mouse position
+			oldMouse3DPos = returnMouseRelativePos();
+			//return true if we got a node
+			return (selectedNode != 0);
+			break;
+
+		case EMIE_LMOUSE_LEFT_UP:
+			LMouseDown = false;
+			return false;
+			break;
+
+		case EMIE_MOUSE_MOVED:
+			//return if we aren't dragging
+			if (LMouseDown == false)
+				return false;
+			//see if we have a node
+			if (selectedNode != 0)
+			{
+				//calculate new mouse pos
+				core::vector3df mouse3DPos = returnMouseRelativePos();
+				//move the node by the difference of the two
+				selectedNode->setPosition(selectedNode->getPosition() + (mouse3DPos - oldMouse3DPos));
+				//update old pos
+				oldMouse3DPos = mouse3DPos;
+			}
+			break;
+		}
+	}
 	return false;
 }
--- Orbitersdk/shipyard/Shipyard.h
+++ Orbitersdk/shipyard/Shipyard.h
@@ -9,10 +9,17 @@ using namespace irr;
 class Shipyard : public IEventReceiver
 {
 public:
-	void setupDevice(IrrlichtDevice * _device) { device = _device; }
+	void setupDevice(IrrlichtDevice * _device);
 	void loop();
 	bool OnEvent(const SEvent & event);
 
 private:
+	core::vector3df returnMouseRelativePos();
+	
 	IrrlichtDevice * device;
+	bool LMouseDown;
+	scene::ISceneNode * selectedNode;
+	scene::ISceneCollisionManager* collisionManager;
+	scene::ISceneManager* smgr;
+	core::vector3df oldMouse3DPos;
 };
