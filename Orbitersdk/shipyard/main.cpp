59a94d8aca8caaf3805b5a97b2164fe1d66e94dc
TheNewBob <b_haefeli@gmx.ch>
2014-03-05 18:34:00 +0100

    Just committing this because I have to roll back a step

--- Orbitersdk/shipyard/main.cpp
+++ Orbitersdk/shipyard/main.cpp
@@ -1,4 +1,4 @@
-#include <irrlicht.h>
+#include "Common.h"
 #include <algorithm>
 #include "Shipyard.h"
 #include "Helpers.h"
@@ -14,16 +14,51 @@ using namespace irr;
 int main()
 {
 
-	// create a NULL device to detect screen resolution
-	IrrlichtDevice *nulldevice = createDevice(video::EDT_NULL);
+	//loading configuration from StackEditor.cfg
+	core::dimension2d<u32> windowRes = dimension2d<u32>(0,0);
+	std::string tbxSet("default");
+	vector<std::string> tokens;
+	std::string cfgPath = std::string(Helpers::workingDirectory + "\\StackEditor\\StackEditor.cfg");
+	ifstream configFile = ifstream(cfgPath.c_str());
+	if (configFile)
+	{
+		while (Helpers::readLine(configFile, tokens))
+		{
+			if (tokens.size() == 0) continue;
 
-//	core::dimension2d<u32> deskres = nulldevice->getVideoModeList()->getDesktopResolution();
-	core::dimension2d<u32> deskres = core::dimension2d<u32>(1366, 710);
-	nulldevice->drop();
+			if (tokens[0].compare("resolution") == 0 && tokens.size() == 3)
+			{
+				windowRes = dimension2d<u32>(UINT(Helpers::stringToInt(tokens[1])), UINT(Helpers::stringToInt(tokens[2])));
+			}
+
+			if (tokens[0].compare("tbxset") == 0) 
+			{
+				if (tokens.size() > 2)
+				//putting together the rest of the string again if the folder name contains a space
+				for (UINT i = 1; i < tokens.size(); ++i)
+				{
+					tbxSet += std::string(tokens[i] + " ");
+				}
+				else
+				{
+					tbxSet = tokens[1];
+				}
+			}
+		}
+	}
+
+	if (windowRes == dimension2d<u32>(0, 0))
+	//resolution not specified, create a NULL device to detect screen resolution
+	{
+		IrrlichtDevice *nulldevice = createDevice(video::EDT_NULL);
+		windowRes = nulldevice->getVideoModeList()->getDesktopResolution();
+		nulldevice->drop();
+	}
+	
 
 	Shipyard shipyard = Shipyard();
 
-	IrrlichtDevice *device = createDevice(video::EDT_DIRECT3D9, deskres, 32, false, false, false, &shipyard);
+	IrrlichtDevice *device = createDevice(video::EDT_DIRECT3D9, windowRes, 32, false, false, false, &shipyard);
 	if (!device)
 		return 1;
 
